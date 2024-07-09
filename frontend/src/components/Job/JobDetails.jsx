988b76724dd46413eef66cae1e1ec5e43e07684a
Sairam2614 <111753996+Sairam2614@users.noreply.github.com>
2024-07-10 03:32:31 +0530

    Update JobDetails.jsx backend url

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -14,7 +14,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`https://mern-job-application-1.onrender.com/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
